11c35464151f5233e71eb9774122a182a94a1747
Michael Kolodji <kolodjimichael@gmail.com>
2022-11-12 16:57:49 -0300

    Eureka fix project name

--- eureka-service/src/main/java/com/mk/microservices/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/mk/microservices/eurekaservice/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mk.microservices.eurkaservice;
+package com.mk.microservices.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurkaServiceApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurkaServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
